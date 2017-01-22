c03d5a2a595851670963ac9f2214f8c862299f42
Thahir Maheen <thahir@farabi.ae>
2017-01-22 17:40:08 +0400

    add required files

---
+++ Example/Pods/Target Support Files/TMRaptureXML/TMRaptureXML-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TMRaptureXML : NSObject
+@end
+@implementation PodsDummy_TMRaptureXML
+@end
---
+++ Example/Pods/Target Support Files/TMRaptureXML/TMRaptureXML-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double TMRaptureXMLVersionNumber;
+FOUNDATION_EXPORT const unsigned char TMRaptureXMLVersionString[];
+
